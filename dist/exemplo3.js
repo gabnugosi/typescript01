45ae09860fd639563e88acbe3493acf965e34f20
Gabriel Nunes <nunes.gabriel.13@gmail.com>
2019-07-26 22:29:21 -0300

    Exemplo 3 TS

---
+++ dist/exemplo3.js
@@ -0,0 +1,4 @@
+var personagemPreferido; //tipo any aceita qualquer valor
+personagemPreferido = "Sasuke";
+personagemPreferido = 789785;
+console.log("Meu personagem preferido é " + personagemPreferido);
